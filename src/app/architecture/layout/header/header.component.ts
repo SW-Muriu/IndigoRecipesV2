c1e7a5c8f327ef5fef6a5c5b7ad1790d6eae3417
SW-Muriu <sw.muriu@gmail.com>
2024-04-25 09:23:22 +0300

    Completed the configuration of the header

--- src/app/architecture/layout/header/header.component.ts
+++ src/app/architecture/layout/header/header.component.ts
@@ -1,12 +1,65 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { SharedModule } from '../../modules/shared.module';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    SharedModule
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  
+  currentUserName!: string | null;
+  currentUserEmail!: string | null;
+  hideSearch: string | null = this.currentUserName;
+  shouldDeferContent: boolean = false;
+
+  constructor(
+    private router: Router,
+
+  ) {
+   }
+
+
+  ngOnInit(): void {
+    this.currentUserName = sessionStorage.getItem('username');
+    this.currentUserEmail = sessionStorage.getItem('email');
+
+  }
+
+
+  /**** Adding a recipe */
+  addRecipe(): void {
+    console.log("Adding Recipe");
+    let route = 'manage/recipe';
+    this.router.navigate([route]);
+  }
+
+  /**** Navigate back to home */
+  navigateToHome(): void {
+    let route = '/home'
+    this.router.navigate([route]);
+  }
+
+  /**** Logging out */
+  onLogout(): void {
+    const route = '/#';
+    sessionStorage.removeItem("username");
+    this.router.navigate([route]);
+  }
+
+  updateProfile(): void {
+    let route = '/profile/update';
+    this.router.navigate([route]);
+  }
 
 }
+
+
+
+
+
