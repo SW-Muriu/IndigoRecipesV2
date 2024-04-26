09f3ed2f070aeb8c4d921a4c274f0115c1207613
SW-Muriu <sw.muriu@gmail.com>
2024-04-26 16:37:53 +0300

    Configured the footer component

---
+++ src/app/architecture/layout/footer/footer.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { SharedModule } from '../../modules/shared.module';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [
+    SharedModule,
+  ],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss'
+})
+export class FooterComponent {
+ 
+  mngForm: FormGroup
+
+  constructor(
+    private fb: FormBuilder, 
+  ) {
+    this.mngForm = this.fb.group({
+      response: ['', [Validators.required, Validators.maxLength(3000)]],
+    })
+  }
+
+  onSendEmail(): void {
+    if (this.mngForm.value.response != null) {
+      const username = sessionStorage.getItem("username");
+      // this.notificationManService.showNotificationMessage(`Thank you ${username} for your feedback`, 'snackbar-success');
+    } else {
+      // this.notificationManService.showNotificationMessage(`Kindly fill the form`, 'snackbar-success');
+    }
+  }
+}
