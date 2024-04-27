b03fbd6392c00135477d1c1050476721c049337d
SW-Muriu <sw.muriu@gmail.com>
2024-04-27 08:45:08 +0300

    Managed the notification services

---
+++ src/app/architecture/services/notification/notification.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotificationService {
+
+  constructor(
+    public snackbar: MatSnackBar,
+  ) { }
+
+  horizontalPosition: MatSnackBarHorizontalPosition = "end";
+  verticalPosition: MatSnackBarVerticalPosition = "top";
+
+  /** Show Success and Error notifications */
+  showNotificationMessage(
+    message: string,
+    type: 'snackbar-danger' | 'snackbar-success' | 'login-snackbar',
+    duration: number = 500,
+  ): void {
+    this.snackbar.open(message, 'X', {
+      duration,
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+      panelClass: [type]
+    });
+  }
+}
